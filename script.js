18900c40a7024e04bb250b5a99e85b964540fb96
VishwanathSinghYadav <singhvishwanathyadav@gmail.com>
2022-02-01 22:03:49 +0530

    Added content and function.

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let firstCard = 3;
 let secondCard = 10;
 let sum = firstCard + secondCard;
+message = "";
+let startEl = document.getElementById("startGame()");
+
+function startGame() {
+
+    if (sum < 21) {
+        message = "Want to add an extra card?";
+    } else if (sum === 21) {
+        message = "That's a Blackjack!!! ♠ "
+    } else {
+        message = "You are out of luck. Try again? "
+    }
+
+    console.log(message);
+}
