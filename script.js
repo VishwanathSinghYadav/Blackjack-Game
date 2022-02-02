be2011051e9af503f3e40cc0429cfa5623f7b314
VishwanathSinghYadav <singhvishwanathyadav@gmail.com>
2022-02-02 21:04:40 +0530

    added functionality

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let firstCard = 3;
+let firstCard = 11;
 let secondCard = 10;
 let sum = firstCard + secondCard;
-message = "";
-let startEl = document.getElementById("startGame()");
+let message = "";
+let messageEl = document.getElementById("message-id");
+let titleEl = document.getElementById("title-id");
 
 function startGame() {
-
+    titleEl.style.visibility = "hidden";
     if (sum < 21) {
         message = "Want to add an extra card?";
     } else if (sum === 21) {
-        message = "That's a Blackjack!!! ♠ "
+        message = "That's a Blackjack!!! \n ♠ "
     } else {
         message = "You are out of luck. Try again? "
     }
 
-    console.log(message);
+    messageEl.innerText = message;
+
 }
